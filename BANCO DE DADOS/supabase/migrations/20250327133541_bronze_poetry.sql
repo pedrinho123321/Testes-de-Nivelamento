8dfb35692a1ef2b3c88f2b1e84a917dbe4d0c4b6
Pedro Vasconcelos <75330398+pedrinho123321@users.noreply.github.com>
2025-03-30 11:13:27 -0300

    Update 20250327133541_bronze_poetry.sql

--- BANCO DE DADOS/supabase/migrations/20250327133541_bronze_poetry.sql
+++ BANCO DE DADOS/supabase/migrations/20250327133541_bronze_poetry.sql
@@ -1,4 +1,3 @@
--- Importação dos dados das operadoras
 COPY operadoras FROM '/data/operadoras_ativas.csv'
 WITH (
     FORMAT csv,
@@ -7,8 +6,6 @@ WITH (
     ENCODING 'UTF8'
 );
 
--- Importação dos dados das demonstrações contábeis
--- Nota: Este é um template, os nomes dos arquivos precisarão ser especificados
 COPY demonstracoes_contabeis FROM '/data/demonstracoes_{ANO}_{TRIMESTRE}.csv'
 WITH (
     FORMAT csv,
